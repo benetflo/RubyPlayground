d3af3a80ace6efcb598002e5ec1a726f379a8ee0
benflo <benji@DESKTOP-JQVMOHH.>
2025-03-03 19:30:52 +0100

    fixed threading on server

--- networking/simple_tcp_server.rb
+++ networking/simple_tcp_server.rb
@@ -1,4 +1,6 @@
 require 'socket'
+require 'thread'
+
 
 IP = "0.0.0.0" # allow all
 PORT = 12345
@@ -10,21 +12,26 @@ begin
 
 
 	loop do
-		begin
-			client = server.accept
-			puts "Connection from #{client}"
-			msg_from_client = client.read(BYTES_CAP)
-			if msg_from_client
-				puts "Message from client #{msg_from_client.inspect}"
-			else
-				puts "No message was recieved from client"
-			end
 
-		rescue => e
-			puts "ERROR: #{e.message}"
-		ensure
-			client.close if client
+		client = server.accept
+		puts "[#{Time.now}]: Connection from #{client.peeraddr[2]}" # client.peeraddr returns info about client connection as: array[ADDRESS_FAMILY,PORT, IP, SAME IP]. peeraddr[2] == Clients IP-address 
+		Thread.new(client) do |conn| # create new thread for each connection
+			begin
+				msg_from_client = conn.read(BYTES_CAP) # read data from current connection
+				if msg_from_client
+					puts "Message from client #{msg_from_client.inspect}"
+					conn.puts "Server sucessfully recieved message"
+				else
+					puts "No message was recieved from client"
+				end
+
+			rescue => e
+				puts "ERROR: #{e.message}"
+			ensure
+				conn.close # close current connection
+			end
 		end
+	end
 rescue => e
 	puts "ERROR: #{e.message}"
 ensure
@@ -32,4 +39,4 @@ ensure
 end
 
 
-#TODO: Add threading
+
